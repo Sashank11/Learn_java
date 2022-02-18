8e40d531669084c592cd7bdeaf671933f903ed38
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-18 21:14:48 +0545

    print nextline

--- intro/HelloWorld.java
+++ intro/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello World!"); // prints hello world
+        System.out.println("Hello World!"); // prints hello world
     }
 }
