907d8d353d9156faf5ab96185bf8efaf85879c45
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-19 15:59:28 +0545

    print equation

--- intro/VarsIntro.java
+++ intro/VarsIntro.java
@@ -1,14 +1,17 @@
 public class VarsIntro {
     public static void main(String[] args) {
 
-        int a = 25;
-        int b = 10;
+        int a = 10;
+        int b = 5;
         int sum = a + b;
         // System.out.println(sum);
         int diff = a - b;
         // System.out.println(diff);
         int mul = a * b;
         System.out.println(mul);
+
+        int eq = (a * b) / (a - b);
+        System.out.println(eq);
 
 
     }
