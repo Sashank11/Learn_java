dcf28a755c06dcb353352faccc32cc0a98120b24
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-19 15:48:10 +0545

    print sum

--- intro/VarsIntro.java
+++ intro/VarsIntro.java
@@ -1,5 +1,11 @@
 public class VarsIntro {
+    public static void main(String[] args) {
 
-    
-    
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        System.out.println(sum);
+
+
+    }
 }
