6cfef415f99827acda47ba9885a5b202f056ca8e
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-20 13:02:48 +0545

    OddEven

--- intro/Conditions.java
+++ intro/Conditions.java
@@ -4,13 +4,20 @@ public class Conditions {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
-
-        if (age  > 18){
-            System.out.println("Adult");
+        // int age = sc.nextInt();
+        // if (age  > 18){
+        //     System.out.println("Adult");
+        // }
+        // else{
+        //     System.out.println("Not Adult");
+        // }
+        int x = sc.nextInt();
+        sc.close();
+        if (x % 2 == 0 ){
+            System.out.println("Even");
         }
         else{
-            System.out.println("Not Adult");
+            System.out.println("Odd");
         }
     }
 }
