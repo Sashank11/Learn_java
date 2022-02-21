c8810e9193ff4dacd63f02dce5665e7c3a8d01cd
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-21 07:33:29 +0545

    0-10 while

--- loops/Loops.java
+++ loops/Loops.java
@@ -3,8 +3,13 @@ import java.util.*;
 public class Loops {
     
     public static void main(String[] args){
-        for(int i = 0; i < 11; i++){
+        // for(int i = 0; i < 11; i++){
+        //     System.out.println(i);
+        // }
+        int i = 0;
+        while(i < 11){
             System.out.println(i);
+            i++;
         }
         
     }
