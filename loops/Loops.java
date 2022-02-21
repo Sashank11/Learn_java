fd84506379633ac79f775c078f562f80d60db5de
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-21 07:27:42 +0545

    loop 0-10

--- loops/Loops.java
+++ loops/Loops.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class Loops {
     
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 11; i++){
+            System.out.println(i);
+        }
         
     }
 }
