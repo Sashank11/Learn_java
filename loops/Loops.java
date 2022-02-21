d86a491851dbfca8f27d34b116e605e5a945469a
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-21 07:43:48 +0545

    n natural numbers

--- loops/Loops.java
+++ loops/Loops.java
@@ -11,11 +11,18 @@ public class Loops {
         //     System.out.println(i);
         //     i++;
         // }
-        int i = 0;
-        do{
-            System.out.println(i);
-            i++;
-        } while(i < 11);
-        
-    }
+        // int i = 0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // } while(i < 11);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        int sum = 0;
+        for(int i = 1; i <=n; i++){
+            sum = sum + i;
+        }
+        System.out.println("Sum: " + sum);
+    }   
 }
