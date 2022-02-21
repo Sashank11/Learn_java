e00537468625701f084f07efcefba8f170fab690
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-21 07:37:40 +0545

    0-10 do-while

--- loops/Loops.java
+++ loops/Loops.java
@@ -6,11 +6,16 @@ public class Loops {
         // for(int i = 0; i < 11; i++){
         //     System.out.println(i);
         // }
+        // int i = 0;
+        // while(i < 11){
+        //     System.out.println(i);
+        //     i++;
+        // }
         int i = 0;
-        while(i < 11){
+        do{
             System.out.println(i);
             i++;
-        }
+        } while(i < 11);
         
     }
 }
