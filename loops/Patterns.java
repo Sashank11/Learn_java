a719c59eb3fb6767073829cdd409804cc23515ba
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-21 07:58:30 +0545

    Solid Rectangle

---
+++ loops/Patterns.java
@@ -0,0 +1,19 @@
+public class Patterns {
+
+    public static void main(String[] args){
+
+        //Solid Rectangle
+        int l = 4;
+        int b = 5;
+        for(int i = 1; i<=l; i ++){
+            for(int j = 1; j<=b; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        // Hollow Rectangle
+        
+
+    }
+
+}
