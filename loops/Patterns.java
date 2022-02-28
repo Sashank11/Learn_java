9fb2e1e052acb7c072a611210934c5f8631c05ed
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-28 14:39:20 +0545

    inverted half pyramid

--- loops/Patterns.java
+++ loops/Patterns.java
@@ -27,10 +27,11 @@ public class Patterns {
     //     }
     int n = 4;
     for(int i = 1; i<=n; i++){
-        for(int j = 1; j <=i; j++){
+        for(int j = n; j >=i; j--){
             System.out.print("*");
             }
         System.out.println();
         }
     }
+
 }
