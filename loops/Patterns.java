5495261037a0f23654e94300b4a240d94cfc34f3
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-28 16:15:03 +0545

    01 triangle

--- loops/Patterns.java
+++ loops/Patterns.java
@@ -68,7 +68,19 @@ public class Patterns {
         }
         System.out.println();
     }
-
+    System.out.println();
+    // 0-1 Trianglel
+    for(int i = 1; i<=f; i++){
+        for(int j = 1; j<=i; j++){
+            if((i+j) % 2 == 0){
+                System.out.print(1 + " ");
+            } 
+            else{
+                System.out.print(0 + " ");
+            }
+        }
+        System.out.println();
+    }
 
 
     }
