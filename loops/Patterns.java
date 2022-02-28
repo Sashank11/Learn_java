5ea99878ae19e2ba7d59a4603dc27fd89cc4b880
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-28 14:20:16 +0545

    half pyramid

--- loops/Patterns.java
+++ loops/Patterns.java
@@ -2,30 +2,35 @@ public class Patterns {
 
     public static void main(String[] args){
 
-        //Solid Rectangle
-        int l = 4;
-        int b = 5;
-        for(int i = 1; i<=l; i ++){
-            for(int j = 1; j<=b; j++){
-                System.out.print('*');
+    //     //Solid Rectangle
+    //     int l = 4;
+    //     int b = 5;
+    //     for(int i = 1; i<=l; i ++){
+    //         for(int j = 1; j<=b; j++){
+    //             System.out.print('*');
+    //         }
+    //         System.out.println();
+    //     }
+    //     // Hollow Rectangle
+    //     int n = 4;
+    //     int m = 5;
+    //     for(int i = 1; i<=n; i++){
+    //         for(int j = 1; j <=m; j++){
+    //             if (i ==1 || j == 1 || i == n || j == m){
+    //                 System.out.print("*");
+    //             }
+    //             else{
+    //                 System.out.print(" ");
+    //             }      
+    //         }
+    //         System.out.println();
+    //     }
+    int n = 4;
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j <=i; j++){
+            System.out.print("*");
             }
-            System.out.println();
+        System.out.println();
         }
-        // Hollow Rectangle
-        int n = 4;
-        int m = 5;
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j <=m; j++){
-                if (i ==1 || j == 1 || i == n || j == m){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }      
-            }
-            System.out.println();
-        }
-
     }
-
 }
