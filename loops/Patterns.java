e4e0a90f681e89bda63781a604f811bae697f38f
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-28 15:47:58 +0545

    Floyd's Triangle

--- loops/Patterns.java
+++ loops/Patterns.java
@@ -57,5 +57,19 @@ public class Patterns {
         }
         System.out.println();
     }
+    System.out.println();
+    // Floyd's Triangle
+    int f = 5;
+    int num = 1;
+    for(int i = 1; i<=f; i++){
+        for(int j = 1; j<=i; j++){
+            System.out.print(num + " ");
+            num++;
+        }
+        System.out.println();
+    }
+
+
+
     }
 }
