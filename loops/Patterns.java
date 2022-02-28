d0026d593e7a48363e7159f3ce5aac2edc0ce925
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-28 15:30:19 +0545

    number pattern

--- loops/Patterns.java
+++ loops/Patterns.java
@@ -44,6 +44,18 @@ public class Patterns {
             }
         System.out.println();
         }
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
     }
 }
-
