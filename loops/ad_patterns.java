da5647c46546d46edb39a93c872c6eccee3b2531
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-01 22:13:01 +0545

    Diamond pattern

--- loops/ad_patterns.java
+++ loops/ad_patterns.java
@@ -26,7 +26,7 @@ public class ad_patterns {
             System.out.println();        
         }
     // Rhombus
-    n = 6;
+    n = 4;
     for (int i = 1; i<=n; i++ ){
         for(int j = 1; j<=n-i; j++){
             System.out.print(" ");
@@ -54,6 +54,27 @@ public class ad_patterns {
         }
         for(int j = 2; j<=i; j++){
             System.out.print(j);
+        }
+        System.out.println();
+    }
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j<=(2*i-1); j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for(int i = n; i>=1; i--){
+        for(int j = 1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        for(int j = 1; j<=i; j++){
+            System.out.print("*");
+        }
+        for(int j = 2; j<=i; j++){
+            System.out.print("*");
         }
         System.out.println();
     }
