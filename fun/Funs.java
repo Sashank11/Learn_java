da1da0ac3a8a439ed1b7f2a6c77cbf8f13352fcf
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-04 19:08:23 +0545

    calFac_corrected

--- fun/Funs.java
+++ fun/Funs.java
@@ -7,11 +7,26 @@ public class Funs {
     public static int calProd(int a, int b){
         return a * b;
     }
+    public static int calFac(int a){
+        if (a <0){
+            System.out.println("Factorial for negative doesnot exist");
+            return 0;
+        }
+        else if(a == 0){
+            return 1;
+        }
+        else{
+            int fac = 1;
+            for(int i = a; i>=1; i--){
+                fac = fac * i;
+            }
+        return fac;
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
         sc.close();
-        System.out.println(calProd(a, b));
+        System.out.println(calFac(a));
     }
 }
