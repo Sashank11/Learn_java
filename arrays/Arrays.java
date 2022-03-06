f68e820fa7a35a8b5ce0604827f53781e4d92a75
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-06 12:56:33 +0545

    user defined array size

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -6,13 +6,15 @@ public class Arrays{
         // marks[0] = 97;
         // marks[1] = 98;
         // marks[2] = 95;
-        //or
-        int[] marks = {97, 98, 95};
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        sc.close();
+        int[] numbers = new int[size];
         // System.out.println(marks[0]);
         // System.out.println(marks[1]);
         // System.out.println(marks[2]);
-        for(int i = 0; i<3; i ++){
-            System.out.println(marks[i]);
+        for(int i = 0; i<size; i ++){
+            System.out.println(numbers[i]);
         }
     }
 }
