a4e421b0de7f78861d70442228cac28f8de3c865
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-06 11:34:12 +0545

    marks_loop

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -6,8 +6,11 @@ public class Arrays{
         marks[0] = 97;
         marks[1] = 98;
         marks[2] = 95;
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        for(int i = 0; i<3; i ++){
+            System.out.println(marks[i]);
+        }
     }
 }
