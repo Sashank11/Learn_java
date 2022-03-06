a19981e27aaa0fc2ccce42c8dd945616821fc08d
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-06 13:03:21 +0545

    linear search

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -9,11 +9,16 @@ public class Arrays{
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] numbers = new int[size];
+
         for(int i = 0; i<size; i++){
             numbers[i] = sc.nextInt();
         }
-        for(int i = 0; i<size; i ++){
-            System.out.println(numbers[i]);
+        int x = sc.nextInt();
+        
+        for(int i = 0; i<numbers.length; i++){
+            if(numbers[i] == x){
+                System.out.println("x found at index: " + i);
+            }
         }
         sc.close();
     }
