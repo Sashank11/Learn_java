b7ea8f441bd92b53b41ad23d97701f5b14242150
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-06 12:59:07 +0545

    loop and input array elements

--- arrays/Arrays.java
+++ arrays/Arrays.java
@@ -8,13 +8,13 @@ public class Arrays{
         // marks[2] = 95;
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        sc.close();
         int[] numbers = new int[size];
-        // System.out.println(marks[0]);
-        // System.out.println(marks[1]);
-        // System.out.println(marks[2]);
+        for(int i = 0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
         for(int i = 0; i<size; i ++){
             System.out.println(numbers[i]);
         }
+        sc.close();
     }
 }
