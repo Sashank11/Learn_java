3050d0baa0449c77fa2a29525b00ab02766b0159
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-08 18:20:17 +0545

    length of string

--- strings/Strings.java
+++ strings/Strings.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class Strings {
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Name: " + name);
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println("Name: " + name);
 
         String a = "abc";
         String b = "def";
         String c = a + b;
-        System.out.println(c);
+        System.out.println(c.length());
     }
 }
