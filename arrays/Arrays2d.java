bc406a16d55d49d0022b099b63631c25ce4e38ac
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-08 17:42:09 +0545

    for Output

--- arrays/Arrays2d.java
+++ arrays/Arrays2d.java
@@ -12,7 +12,12 @@ public class Arrays2d {
             nums[i][j] = sc.nextInt();
         }
     }
-
+    for (int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            System.out.print(nums[i][j] + " ");
+        }
+        System.out.println();
+    }
 
     sc.close();
 
