68ef92f73d2825a19b2689cb193bd9835e865faa
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-08 17:37:54 +0545

    2d array init

--- arrays/Arrays2d.java
+++ arrays/Arrays2d.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Arrays2d {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        sc.close();
+
+        int[ ] [ ] nums = new int[rows][cols];
         
     }
 }   
