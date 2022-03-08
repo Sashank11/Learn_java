649271e3198bbe3cd63e88547e3cfe3218f7cc78
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-08 17:40:06 +0545

    for input

--- arrays/Arrays2d.java
+++ arrays/Arrays2d.java
@@ -4,9 +4,17 @@ public class Arrays2d {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
         int cols = sc.nextInt();
-        sc.close();
 
         int[ ] [ ] nums = new int[rows][cols];
-        
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j< cols; j++){
+            nums[i][j] = sc.nextInt();
+        }
+    }
+
+
+    sc.close();
+
     }
 }   
