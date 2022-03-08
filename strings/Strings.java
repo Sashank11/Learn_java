e57c9e77f7466cdcb7d4101bdade3d749bdf0d7d
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-08 18:10:05 +0545

    Input a string

--- strings/Strings.java
+++ strings/Strings.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class Strings {
     public static void main(String[] args){
 
-        String name = "Neo";
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Name: " + name);
 
     }
 }
