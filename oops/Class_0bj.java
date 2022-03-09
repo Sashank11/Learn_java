3a24ff28e4cc805343fe042cd4a48ef069aafe13
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-09 16:35:56 +0545

    Oops init

--- oops/Class_0bj.java
+++
@@ -1,5 +0,0 @@
-public class Class_0bj{
-    public static void main(String[] args){
-        
-    }
-}
---
+++ oops/StudentUse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StudentUse {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int roll_no = sc.nextInt();
+        Student s1 = new Student();
+
+
+
+        sc.close();
+    }
+}
