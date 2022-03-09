8cf22e1668a33faf69e39ded71420047029027af
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-09 16:43:54 +0545

    obj s2

--- oops/StudentUse.java
+++ oops/StudentUse.java
@@ -9,7 +9,10 @@ public class StudentUse {
         s1.name = name;
         s1.roll_no = roll_no;
         System.out.println(s1.name + " " + s1.roll_no);
-
+        Student s2 = new Student();
+        System.out.println(s2);
+        s2.name = "abc";
+        s2.roll_no = 22;
 
         sc.close();
     }
