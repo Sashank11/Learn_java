19a177bec374a0e3c64d852ba89cb7ae9256d830
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-09 16:42:53 +0545

    obj s1

--- oops/StudentUse.java
+++ oops/StudentUse.java
@@ -5,7 +5,10 @@ public class StudentUse {
         String name = sc.nextLine();
         int roll_no = sc.nextInt();
         Student s1 = new Student();
-
+        System.out.println(s1);
+        s1.name = name;
+        s1.roll_no = roll_no;
+        System.out.println(s1.name + " " + s1.roll_no);
 
 
         sc.close();
