7c4a693b52cf2b9d57453dacd098be3d62ad41d2
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-09 10:50:08 +0545

    insert

--- strings/Stringbuild.java
+++ strings/Stringbuild.java
@@ -4,7 +4,10 @@ public class Stringbuild {
         System.out.println(sb);
         System.out.println(sb.charAt(0));
         
-        sb.setCharAt(0, 'S');
+        // sb.setCharAt(0, 'S');
+        // System.out.println(sb);
+        sb.insert(0, "S");
         System.out.println(sb);
+
     }
 }
