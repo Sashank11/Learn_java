551c1bf517fd79a968f77489701cdd3edce1ebef
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-09 10:47:01 +0545

    charAt

--- strings/Stringbuild.java
+++ strings/Stringbuild.java
@@ -1,5 +1,7 @@
 public class Stringbuild {
     public static void main(String[] args){
         StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+        System.out.println(sb.charAt(0));
     }
 }
