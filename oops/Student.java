c9953cfe3c2ac5bb714ab73e673fe27df40f2285
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-16 16:17:18 +0530

    set roll_no

--- oops/Student.java
+++ oops/Student.java
@@ -2,5 +2,12 @@ public class Student{
 
     public String name;
     int roll_no;
-    
+
+
+    public void setRollNumber(int roll_no){
+        if(roll_no <= 0){
+            return;
+        }
+        this.roll_no = roll_no;
+    }   
 }
