6ad8a646ecb153744f96419b5344dec535292152
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-16 16:18:54 +0530

    implement set rollno

--- oops/StudentUse.java
+++ oops/StudentUse.java
@@ -3,16 +3,15 @@ public class StudentUse {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
-        int roll_no = sc.nextInt();
         Student s1 = new Student();
         System.out.println(s1);
         s1.name = name;
-        s1.roll_no = roll_no;
+        s1.setRollNumber(100);
         System.out.println(s1.name + " " + s1.roll_no);
         Student s2 = new Student();
         System.out.println(s2);
         s2.name = "abc";
-        s2.roll_no = 22;
+        s2.setRollNumber(101);
         System.out.println(s2.name + " " + s2.roll_no);
 
         sc.close();
