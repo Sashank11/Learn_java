cb69f1fe612d2b89b6631a59de498325d28ede41
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-16 16:56:10 +0530

    creating constructor

--- oops/Student.java
+++ oops/Student.java
@@ -2,7 +2,10 @@ public class Student{
 
     public String name;
     int roll_no;
-
+    public Student(String name, int roll_no) {
+        this.name = name;
+        this.roll_no = roll_no;
+    }
 
     public void setRollNumber(int roll_no){
         if(roll_no <= 0){
--- oops/StudentUse.java
+++ oops/StudentUse.java
@@ -1,18 +1,13 @@
 import java.util.*;
 public class StudentUse {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        Student s1 = new Student();
+        Student s1 = new Student("Sashank", 11);
         System.out.println(s1);
-        s1.name = name;
-        s1.setRollNumber(100);
+        //s1.setRollNumber(100);
         System.out.println(s1.name + " " + s1.roll_no);
-        Student s2 = new Student();
+        Student s2 = new Student("Neo", 12);
         System.out.println(s2);
-        s2.name = "abc";
-        s2.setRollNumber(101);
+        //s2.setRollNumber(101);
         System.out.println(s2.name + " " + s2.roll_no);
-        sc.close();
     }
 }
