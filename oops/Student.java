b0653b57033e3f99ad62bd95473c04f17b17b4e9
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-20 16:55:11 +0530

    final init

--- oops/Student.java
+++ oops/Student.java
@@ -5,6 +5,8 @@ public class Student{
     public Student(String name, int roll_no) {
         this.name = name;
         this.roll_no = roll_no;
+        double cpgpa;
+        final double conversionFactor = 0.95;
     }
     public Student(String name){
         this.name = name;
