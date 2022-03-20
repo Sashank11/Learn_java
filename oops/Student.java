21063355bbaf8f6d57f19f24683c655c75c6da99
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-20 18:11:30 +0530

    static init

--- oops/Student.java
+++ oops/Student.java
@@ -4,10 +4,11 @@ public class Student{
     final int roll_no;
     double cpgpa;
     final double conversionFactor = 0.95;
-    int num_students;
+    static int num_students;
     public Student(String name, int roll_no) {
         this.name = name;
         this.roll_no = roll_no;
+        num_students++;
     }
     // public Student(String name){
     //     this.name = name;
