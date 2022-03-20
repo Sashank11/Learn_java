0e541632ea136bdc8d624e8b4c1876180213c3a6
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-20 17:07:45 +0530

    final

--- oops/Student.java
+++ oops/Student.java
@@ -8,16 +8,16 @@ public class Student{
         this.name = name;
         this.roll_no = roll_no;
     }
-    public Student(String name){
-        this.name = name;
-    }
+    // public Student(String name){
+    //     this.name = name;
+    // }
     public void print(){
         System.out.println(name + " " + roll_no);
     }
-    public void setRollNumber(int roll_no){
-        if(roll_no <= 0){
-            return;
-        }
-        this.roll_no = roll_no;
-    }   
+    // public void setRollNumber(int roll_no){
+    //     if(roll_no <= 0){
+    //         return;
+    //     }
+    //     this.roll_no = roll_no;
+    // }   
 }
