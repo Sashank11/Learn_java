394081845fa0576ffda70a1834bd70d9af3427b7
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-20 18:33:00 +0530

    prac question

--- oops/StudentUse.java
+++ oops/StudentUse.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class StudentUse {
     public static void main(String[] args){
-        Student s1 = new Student("Sashank", 11);
+        Student s1 = new Student("Sashank");
         System.out.println(s1);
         //s1.setRollNumber(100);
         // s1.print();
-        Student s2 = new Student("Neo", 12);
+        Student s2 = new Student("Neo");
         // s2.print();
         //s2.setRollNumber(101);
         
-        Student s3 = new Student("Samyo", 14);
+        Student s3 = new Student("Samyo");
         // System.out.println(s2.num_students);
         System.out.println(Student.num_students);
 
