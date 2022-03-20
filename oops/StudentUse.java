20329a03abd80467bcc4816449b13d30efa06a2a
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-20 18:12:46 +0530

    print num_students

--- oops/StudentUse.java
+++ oops/StudentUse.java
@@ -4,10 +4,11 @@ public class StudentUse {
         Student s1 = new Student("Sashank", 11);
         System.out.println(s1);
         //s1.setRollNumber(100);
-        s1.print();
+        // s1.print();
         Student s2 = new Student("Neo", 12);
-        s2.print();
+        // s2.print();
         //s2.setRollNumber(101);
+        System.out.println(s2.num_students);
 
     }
 }
