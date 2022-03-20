c3ad01a571cb3f026486a3c1ca3d292d12d8698f
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-20 18:34:03 +0530

    delete test student

--- oops/StudentUse2.java
+++
@@ -1,9 +0,0 @@
-public class StudentUse2 {
-    
-    public static void main(String[] args){
-        Student s = new Student("hello", 22);
-        s.name = "Sashank";
-        System.out.println(s.name);
-    }
-    
-}
