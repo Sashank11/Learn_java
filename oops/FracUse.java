ae99505d459e5eb6f4e7c28ea7d0c3a68f3d9e66
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-21 19:38:33 +0530

    testing add

--- oops/FracUse.java
+++ oops/FracUse.java
@@ -1,10 +1,10 @@
 public class FracUse {
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(4,6);
+        Fraction f1 = new Fraction(2,3);
         f1.print();
         // f1.setNumerator;
         // f1.getNumerator;
-        Fraction f2 = new Fraction(3,6);
+        Fraction f2 = new Fraction(1,2);
         Fraction f3 = new Fraction();
         f3.add(f1,f2);
         // f1.increment();
