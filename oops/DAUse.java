59d4e9478af45d0427249cf7d1f964c839e6df1b
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-24 11:02:47 +0530

    checking add

--- oops/DAUse.java
+++ oops/DAUse.java
@@ -7,12 +7,11 @@ public class DAUse {
             d.add(100 + i);
         }
     System.out.println(d.size());
-    System.out.println(d.get(2));
-    d.set(3,170);
-    System.out.println(d.get(3));
-    while(!d.isEmpty()) {}
-        System.out.println(d.removeLast());
-        System.out.println("size - " + d.size());
+    System.out.println(d.get(98));
+    // d.set(3,170);
+    // System.out.println(d.get(3));
+    // while(!d.isEmpty()) {}
+    //     System.out.println(d.removeLast());
     }
 
 }
