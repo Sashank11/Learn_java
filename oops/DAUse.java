025aa34e3e14efa68615790162429b6d78893b07
Sashank Rijal <rijalsashank.11@gmail.com>
2022-03-24 11:14:29 +0530

    check removeLast

--- oops/DAUse.java
+++ oops/DAUse.java
@@ -8,10 +8,12 @@ public class DAUse {
         }
     System.out.println(d.size());
     System.out.println(d.get(98));
-    // d.set(3,170);
-    // System.out.println(d.get(3));
-    // while(!d.isEmpty()) {}
-    //     System.out.println(d.removeLast());
+    d.set(3,170);
+    System.out.println(d.get(3));
+    while(!d.isEmpty()){
+        System.out.println(d.removeLast());
+        System.out.println("size -" + d.size());
     }
 
-}
+    }
+}   
