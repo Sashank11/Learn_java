36426f29f109c71271f38b5d102db6b5b1a88608
Sashank Rijal <rijalsashank.11@gmail.com>
2022-04-19 15:34:19 +0530

    extends

---
+++ oops2/VecUse.java
@@ -0,0 +1,6 @@
+public class VecUse {
+    public static void main(String[] args){
+        Vec v = new Vec();
+        v.print();
+    }
+}
--- prac/Link.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Link{
-
-    public static void main(String args[])
-    {
-        //10/3-->Ishan
-        //String and number wala ni parya thiyo exception ?
-}
