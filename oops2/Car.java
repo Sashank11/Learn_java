f2eae61458bef1e37a6405d416f7555f95343b70
Sashank Rijal <rijalsashank.11@gmail.com>
2022-04-19 16:28:07 +0530

    Carprint()

--- oops2/Car.java
+++ oops2/Car.java
@@ -2,4 +2,8 @@ public class Car extends Vec {
 
     int numDoors;
 
+    public void Carprint(){
+        System.out.println("Vehicle " + " Color " + color + " speed " + maxSpeed + " numDoors " + numDoors);
+    }
+
 }
