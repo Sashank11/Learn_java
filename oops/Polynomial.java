939f21bc267521be9cd523ee9db679959f9c7a41
Sashank Rijal <rijalsashank.11@gmail.com>
2022-04-19 10:11:01 +0530

    prac

--- oops/Polynomial.java
+++ oops/Polynomial.java
@@ -1,3 +1,45 @@
 public class Polynomial {
-    
+    DynamicArray coefficients;
+    public Polynomial(){
+        coefficients = new DynamicArray();
+    }
+    public void setCoefficient(int deg, int coeff){
+        if(deg >= coefficients.size()){
+            for(int i=0; i<coefficients.size(); i++){
+                coefficients.set(i, 0);
+            }
+        }
+        coefficients.set(deg, coeff);
+    }
+    public int getCoefficient(int deg){
+        if(deg >= coefficients.size()){
+            return -1;
+        }
+        return coefficients.get(deg);
+    }
+    public int degree(){
+        return coefficients.size();
+    }
+    public Polynomial add(Polynomial p){
+        for(int i = 0; i < this.degree() && i < p.degree(); i++){
+            this.setCoefficient(i, this.getCoefficient(i) + p.getCoefficient(i));
+        }
+        return this;
+    }
+    public Polynomial multiply(Polynomial p){
+        for(int i = 0; i < p.degree(); i++){
+
+        }
+        return this;
+    }
+    public void print(){
+        for(int i =0; i < this.degree(); i++){
+            if(this.getCoefficient(i) != 0){
+                System.out.println(getCoefficient(i) + "x" + i + " ");
+            }
+        }
+    }
+    public Polynomial subtract(Polynomial second) {
+        return this;
+    }
 }
--- oops/Prac.java
+++ oops/Prac.java
@@ -1,14 +1,19 @@
-class Test{
-    static int marks;
-    void set_marks(int marks)
-    {
-        this.marks=marks;               //Line 1 gives warning not error
+import java.util.*;
+import java.lang.*;
+class Prac{
+    public static void main(String[] args) {
+        // x and y  string/number ->if string -> error || x/y if y is == 0 ->error ;
+        Scanner sc = new Scanner(System.in);
+        try{
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if( y == 0 ){
+                throw new  RuntimeException(); // hera exception haru keke huncha yo part le test case fail bhayo
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+        }
     }
-}
-class Prac {
-    public static void main (String[] args) {
-        Test t=new Test();              
-        t.set_marks(78);                //Line 2
-        System.out.print(Test.marks);   //Line 3
-    }
-}
