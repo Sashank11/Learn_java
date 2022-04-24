cfc3aeea0a7e34dbf68f2110d69961e8051b2275
Sashank Rijal <rijalsashank.11@gmail.com>
2022-04-24 15:30:06 +0530

    super

--- oops2/Car.java
+++ oops2/Car.java
@@ -3,7 +3,8 @@ public class Car extends Vec {
     int numDoors;
 
     public void print(){
-        System.out.println("Car " + " Color " + getColor() + " speed " + maxSpeed + " numDoors " + numDoors);
+        super.print();
+        System.out.println("Car " + " numDoors " + numDoors);
     }
 
 }
