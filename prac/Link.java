aa1bb5eceee5524f3d5d1d95836360364db2e2d3
Sashank Rijal <rijalsashank.11@gmail.com>
2022-05-09 20:33:26 +0530

    fat prac1

---
+++ prac/Link.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Link {
+        public static void main(String[] args) {
+            int i;
+            String name;
+            Scanner sc = new Scanner(System.in);
+            for(int k = 0;k<=2; k++){
+            i = sc.nextInt();
+            System.out.println();
+            name = sc.nextLine();
+            
+            
+            
+            }
+
+        }
+}
