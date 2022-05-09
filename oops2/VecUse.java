e583f4dc9752b6556815f14b3048cf0f1c79ef37
Sashank Rijal <rijalsashank.11@gmail.com>
2022-05-09 14:36:17 +0530

    vec

--- oops2/VecUse.java
+++ oops2/VecUse.java
@@ -1,16 +1,19 @@
 public class VecUse {
     public static void main(String[] args){
+
         Vec v = new Vec();
-        v.setColor("Red");
-        v.print();
-
         Car c = new Car();
-        c.setColor("Black");
-        c.numDoors = 4;
-        c.print();
+        // Vec v = new Vec();
+        // v.setColor("Red");
+        // v.print();
+
+        // Car c = new Car();
+        // c.setColor("Black");
+        // c.numDoors = 4;
+        // c.print();
 
 
-        Bicycle b = new Bicycle();
-        b.print();
+        // Bicycle b = new Bicycle();
+        // b.print();
     }
 }
