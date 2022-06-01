a73b2f8ec001b5b83113c5c618551fb81e751e4b
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-01 10:27:51 +0545

    sum array rec

--- rec/SumArray.java
+++ rec/SumArray.java
@@ -1,3 +1,19 @@
 public class SumArray {
-    
+    public static int suma(int a[]){
+        if (a.length == 0){
+            return 0;
+        }
+        int smallArray[] = new int [a.length-1];
+        for( int i = 1; i<a.length; i++){
+            smallArray[i-1] = a[i];
+        }
+        int hyp = suma(smallArray);
+        int sum = a[0] + hyp;
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        int a[] = {1,2,3,4,5}; 
+        System.out.println(suma(a));
+        }
 }
