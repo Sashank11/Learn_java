524c7f5fbb2521936cff9d428e29b4264de99f71
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-01 22:02:47 +0545

    string replace

--- sorting/Strings.java
+++ sorting/Strings.java
@@ -1,3 +1,18 @@
 public class Strings {
-    
+    public static String replaceChar(String s, char a, char b){
+        if(s.length() == 0){
+            return s;
+        }
+        String smallOutput = replaceChar(s.substring(1), a, b);
+        if(s.charAt(0) == a){
+            return b + smallOutput;
+        }
+        else{
+            return s.charAt(0) + smallOutput;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replaceChar("abcxdxex", 'x', 'y'));
+    }
 }
