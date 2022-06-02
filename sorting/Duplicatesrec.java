4b8ca7b5c5ed0a67c5cccf4154e7bd518fa5c66c
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-02 12:15:08 +0545

    dup java

--- sorting/Duplicatesrec.java
+++ sorting/Duplicatesrec.java
@@ -4,11 +4,14 @@ public class Duplicatesrec {
             return s;
         }
         String smallString = removeConsecutiveDuplicates(s.substring(1));
-        if(s.charAt(0) == smallString.charAt(1)){
+        if(s.charAt(0) == smallString.charAt(0)){
             return smallString;
         }
         else{
             return s.charAt(0) + smallString;
         }
 	}
+    public static void main(String[] args) {
+        System.out.println(removeConsecutiveDuplicates("aaabbbccc"));
+    }
 }
