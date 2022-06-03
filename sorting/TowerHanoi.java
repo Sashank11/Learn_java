f9f7f9e8b8e5d2aaa681089aff46c7f24077ca81
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-03 21:49:06 +0545

    Tower hanoi init

---
+++ sorting/TowerHanoi.java
@@ -0,0 +1,8 @@
+public class TowerHanoi {
+    public static void towerHanoi(int n, char s, char h, char d){
+        if(n == 1){
+            System.out.println("move 1st disk from " + s + " to " + d);
+        }
+        
+    }
+}
