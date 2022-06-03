776b4aacd0643092d799a20939c62560597a3bb5
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-03 22:27:19 +0545

    tower hanoi complete

--- sorting/TowerHanoi.java
+++ sorting/TowerHanoi.java
@@ -2,7 +2,13 @@ public class TowerHanoi {
     public static void towerHanoi(int n, char s, char h, char d){
         if(n == 1){
             System.out.println("move 1st disk from " + s + " to " + d);
+            return;
         }
-        
+        towerHanoi(n-1, s, d, h);
+        System.out.println("move" + n + "th disk from " + s + " to " + d);
+        towerHanoi(n-1,h, s, d);
+    }
+    public static void main(String[] args) {
+        towerHanoi(5, 's', 'h' , 'd');
     }
 }
