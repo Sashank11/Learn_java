4596be6b0bd3ed5ed398333992cb2395ee41f9b8
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-09 21:49:57 +0545

    equilibrium index

--- Complexity/Equilibrium.java
+++ Complexity/Equilibrium.java
@@ -1,3 +1,21 @@
 public class Equilibrium {
-    
+    public static int arrayEquilibriumIndex(int[] arr){  
+		int leftsum = 0;
+        int totalsum = 0;
+        int rightsum = 0;
+        for(int a = 0; a < arr.length; a++){
+            totalsum += arr[a];
+        }
+        for(int i = 0; i<arr.length;i++){
+            rightsum = totalsum - leftsum -arr[i];
+            
+            if(leftsum == rightsum){
+                return i;
+            }
+            leftsum = leftsum +arr[i];
+        }
+        
+        
+        return -1;
+	}
 }
