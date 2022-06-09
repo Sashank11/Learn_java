18880d2c8583a579fd56e396a5ed923e36b787c8
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-09 22:43:52 +0545

    duplicate

---
+++ Complexity/Duplicate.java
@@ -0,0 +1,11 @@
+public class Duplicate {
+	public static int findDuplicate(int[] arr) {
+		//Your code goes here
+		int n=arr.length-2;
+		int sum=0;
+		for(int i : arr) {
+			sum+=i;
+		}
+		return sum-(n*(n+1)/2); //The total sum -  sum of n numbers
+	}
+}
