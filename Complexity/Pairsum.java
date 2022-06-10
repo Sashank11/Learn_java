1e2ab9c6f0a144fa646080e33de272948a1d59ef
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-11 00:00:46 +0545

    pairsum

--- Complexity/Pairsum.java
+++ Complexity/Pairsum.java
@@ -1,6 +1,19 @@
+import java.util.*;
 public class Pairsum {
-    public static int pairSum(int[] arr, int num) {
-		//Your code goes here
-        
+public static int pairSum(int[] arr,int sum) {
+	HashMap<Integer,Integer> m = new HashMap<>();
+    int count = 0;
+    for(int i = 0; i < arr.length; i++){
+        if (m.containsKey(sum - arr[i])) {
+            count += m.get(sum - arr[i]);
+        }
+        if(m.containsKey(arr[i])){
+            m.put(arr[i], m.get(arr[i])+1);
+        }
+        else{
+            m.put(arr[i], 1);
+        }
+    }
+    return count;	
 	}
 }
