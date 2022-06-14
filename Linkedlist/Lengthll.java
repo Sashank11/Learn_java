af31c1b69b38cba40d4611bef422f9a37062829f
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-14 22:13:53 +0545

    length ll

---
+++ Linkedlist/Lengthll.java
@@ -0,0 +1,12 @@
+public class Lengthll {
+    // public static int length(LinkedListNode<Integer> head){
+	// 	//Your code goes here
+    //     Node<Integer> current = head;
+    //     int count = 0;
+    //     while(current != null){
+    //         count++;
+    //         current = current.next;
+    //     }
+    //     return count;
+	// }
+}
