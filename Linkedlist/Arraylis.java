ecee71191a451ba4fcddbb0df8e6893133ed7c19
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-14 13:44:17 +0545

    arrlist dec add() size()

--- Linkedlist/Arraylis.java
+++ Linkedlist/Arraylis.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
 public class Arraylis {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        arr.add(30);
+        System.out.println(arr.size());
+    }
 }
