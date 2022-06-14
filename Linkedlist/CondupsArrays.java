54c7208e4c74b220f2e52fa4325361eb4075f02c
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-14 14:12:49 +0545

    remDups init

---
+++ Linkedlist/CondupsArrays.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CondupsArrays {
+
+    public static ArrayList<Integer> remDups(int arr[]){
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
+        for(int i= 1; i<arr.length; i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
