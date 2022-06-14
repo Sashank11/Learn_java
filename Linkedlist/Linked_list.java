bcf94d3efa365c89dd731ca760b1734f175039c2
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-14 18:17:16 +0545

    node class

---
+++ Linkedlist/Linked_list.java
@@ -0,0 +1,10 @@
+public class Linked_list {
+    public class Node{
+        int data;
+        Node next;  // In java by default its null
+        Node(int data){
+            this.data = data;
+        }
+
+    }
+}
