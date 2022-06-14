3faebb3b08d35b41e10851b9e368fb04250d6ec0
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-14 14:16:02 +0545

    checking remDups()

--- Linkedlist/CondupsArrays.java
+++ Linkedlist/CondupsArrays.java
@@ -6,10 +6,17 @@ public class CondupsArrays {
         ArrayList<Integer> result = new ArrayList<>();
         result.add(arr[0]);
         for(int i= 1; i<arr.length; i++){
-            
+            if(arr[i] != arr[i-1]){
+                result.add(arr[i]);
+            }
         }
+        return result;
     }
     public static void main(String[] args) {
-        
+        int arr[] = {10,10,20,20,20,30,30,40,10};
+        ArrayList<Integer> result = remDups(arr);
+        for(int i : result){
+            System.out.println(i);
+        }
     }
 }
