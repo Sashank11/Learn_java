b4ee208d24650ab7cfa1389e01668c71bec9e982
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-14 18:19:15 +0545

    Rename Node file

---
+++ Linkedlist/LinkListUse.java
@@ -0,0 +1,6 @@
+public class LinkListUse {
+    public static void main(String[] args) {
+        Node n1 = new Node(10);
+        
+    }
+}
--- Linkedlist/Node.java
+++ Linkedlist/Node.java
@@ -1,5 +1,4 @@
-public class Linked_list {
-    public class Node{
+public class Node{
         int data;
         Node next;  // In java by default its null
         Node(int data){
@@ -7,4 +6,3 @@ public class Linked_list {
         }
 
     }
-}
