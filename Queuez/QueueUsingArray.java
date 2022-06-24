55187a1c56cbb3fb566d1ea3dc57d75eac7a2e56
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-24 12:33:31 +0545

    constructor

--- Queuez/QueueUsingArray.java
+++ Queuez/QueueUsingArray.java
@@ -1,3 +1,19 @@
 public class QueueUsingArray {
+    private int data[];
+    private int front; // index of element at the front of the queue
+    private int rear;  //  index of element at the rear of the queue
+    private int size;
+    public QueueUsingArray(){
+        data = new int[5];
+        front = -1;
+        rear = -1;
+    }
+
+        public QueueUsingArray(int capacity){
+        data = new int[capacity];
+        front = -1;
+        rear = -1;
+    }
+
     
 }
