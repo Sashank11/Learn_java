170ba438714da0620d8e27f2a11fda229072cc48
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-24 22:37:09 +0545

    QueueUsingStacks init

---
+++ Queuez/QueueUsingStacks.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class QueueUsingStacks<T> {
+    Stack<T> s1;
+    Stack<T> s2;
+
+    public QueueUsingStacks(){
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+}
