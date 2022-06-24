084643b38bdb0f45903b99cd2b9f29c303b16110
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-24 23:43:20 +0545

    Reverse the First K Elements in the Queue

--- Queuez/RevK.java
+++ Queuez/RevK.java
@@ -1,8 +1,26 @@
 import java.util.*;
 public class RevK {
-    public static Queue<Integer> reverseKElements(Queue<Integer> input, int k) {
+    public static void reverseQueue(Queue<Integer> input) {
 		//Your code goes here
-
+        if(input.isEmpty()){
+            return;
+        }
+        int temp = input.poll();
+        reverseQueue(input);
+        input.add(temp);
+    }
+    public static Queue<Integer> reverseKElements(Queue<Integer> input, int k) {
+        Queue<Integer> output = new LinkedList<Integer>();
+        for(int i = 0; i <k; i++){
+            int temp = input.poll();
+            output.add(temp);
+        }
+        reverseQueue(output);
+        while(!(input.isEmpty())){
+            int t = input.poll();
+            output.add(t);
+        }
+        return output;
 	}
 
 }
