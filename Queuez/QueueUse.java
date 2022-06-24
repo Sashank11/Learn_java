0054b1b342f1fe4af29275ba2013602768031e7e
Sashank Rijal <rijalsashank.11@gmail.com>
2022-06-24 13:07:08 +0545

    Queue Implementation

--- Queuez/QueueUse.java
+++ Queuez/QueueUse.java
@@ -1,3 +1,13 @@
 public class QueueUse {
     
+    public static void main(String[] args) {
+        QueueUsingArray queue = new QueueUsingArray();
+        int arr[] = {1,2,3,4};
+        for(int element : arr){
+            queue.enqueue(element);
+        }
+        while(!queue.isEmpty()){
+            System.out.println(queue.dequeue());
+        }
+    }
 }
